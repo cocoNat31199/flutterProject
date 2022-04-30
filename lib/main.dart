d1307653e8a6faa322badcebfc1850fbf4c7a27e
cocoNat31199 <nattawat31199@gmail.com>
2022-04-30 17:17:43 +0700

    updateUser

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:merrily/screen/controlPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -10,6 +11,8 @@ import 'screen/controlPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
